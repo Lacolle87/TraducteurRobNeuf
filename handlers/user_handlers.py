21c74243ab1ecd065750f939384c3d837b8bbce4
GeorgeVictor666 <120365126+GeorgeVictor666@users.noreply.github.com>
2024-03-22 16:09:43 +0400

    callback edit keyboard on change lang updated lexicon.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -27,9 +27,13 @@ async def start(message: Message):
 
 
 @router.message(Command(commands='change_language'))
-async def start(message: Message):
-    await message.answer('From:', reply_markup=create_language_keyboard(bot_lang_from, prefix='FROM'))
-    await message.answer('To:', reply_markup=create_language_keyboard(bot_lang_to, prefix='TO'))
+async def change_language(message: Message):
+    user_id = str(message.from_user.id)
+    src_name = next((lang for lang, code in bot_lang_from.items() if code == users_config[user_id]['src_lang']))
+    dest_name = next((lang for lang, code in bot_lang_to.items() if code == users_config[user_id]['dest_lang']))
+    await message.answer(MESSAGES['/change_language'])
+    await message.answer(f'From: {src_name}', reply_markup=create_language_keyboard(bot_lang_from, prefix='FROM'))
+    await message.answer(f'To: {dest_name}', reply_markup=create_language_keyboard(bot_lang_to, prefix='TO'))
 
 
 @router.message(Command(commands='help'))
@@ -53,6 +57,8 @@ async def source_lang(callback: CallbackQuery):
     users_config[user_id]['src_lang'] = language_code
     language_name = next((lang for lang, code in bot_lang_from.items() if code == language_code))
     save_users_config(users_config)
+    await callback.message.edit_text(f'Source: {language_name}',
+                                     reply_markup=create_language_keyboard(bot_lang_from, prefix='FROM'))
     await callback.message.answer(f'Source: {language_name}')
 
 
@@ -63,4 +69,6 @@ async def destination_lang(callback: CallbackQuery):
     users_config[user_id]['dest_lang'] = language_code
     language_name = next((lang for lang, code in bot_lang_from.items() if code == language_code), None)
     save_users_config(users_config)
+    await callback.message.edit_text(f'Destination: {language_name}',
+                                     reply_markup=create_language_keyboard(bot_lang_to, prefix='TO'))
     await callback.message.answer(f'Destination: {language_name}')
--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -2,15 +2,27 @@ MESSAGES: dict[str, str] = {
     '/start': 'Bienvenue à «Traducteur Rob Neuf». '
               '\n\nVous pouvez choisir la langue en appuyant sur: \n/change_language \nou utiliser les paramètres par défaut.'
               '\n\nParamètres définis maintenant: ',
-    '/help': 'This is not you average translator bot.'
-             '\nUse /start to start over and over again.'
-             '\nUse /change_language to select source language and destination language.'
-             '\nUse /help for help.',
-    '/change_language': 'Select desired language for translation'
+    '/help': '\nWelcome to our multi-language translator bot!'
+             '\n\nThis is not your average translator bot.'
+             '\n\nTo get a translation, simply type in your message.'
+             '\n\nWe support translation between 9 languages and even have an auto-detect feature for convenience.'
+             '\n\nUse /start to begin a new translation session.'
+             '\n\nUse /change_language to select your preferred source and destination languages from the following options:'
+             '\n\n🇨🇳 Chinese'
+             '\n🇬🇧 English'
+             '\n🇮🇳 Hindi'
+             '\n🇪🇸 Spanish'
+             '\n🇫🇷 French'
+             '\n🇸🇦 Arabic'
+             '\n🇧🇩 Bengali'
+             '\n🇵🇹 Portuguese'
+             '\n🇷🇺 Russian'
+             '\n🌐 Auto Detect',
+    '/change_language': 'Select desired language for translation:'
 }
 
 LEXICON_COMMANDS: dict[str, str] = {
     '/start': 'Limbo. Latin: limbus, «edge» or «boundary».',
     '/help': 'Mayday, mayday. Man overboard again.',
-    '/change_language': 'Selection.'
+    '/change_language': 'Select.'
 }
