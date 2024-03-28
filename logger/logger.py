d403b23caf11bc4277d0b0a8b6f180e6c55e08d6
GeorgeVictor666 <120365126+GeorgeVictor666@users.noreply.github.com>
2024-03-28 11:35:08 +0400

    logs

--- logger/logger.py
+++ logger/logger.py
@@ -9,13 +9,12 @@ def setup_logger(log_file='bot.log'):
     if not os.path.exists(logs_folder):
         os.makedirs(logs_folder)
 
-    log_basename = os.path.splitext(log_file)[0]  # Extracting the base name without extension
     log_path = os.path.join(logs_folder, log_file)
 
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
 
-    handler = TimedRotatingFileHandler(log_path, when='W0', interval=1, backupCount=4)
+    handler = TimedRotatingFileHandler(log_path, when='W0', interval=1, backupCount=3)
     handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
